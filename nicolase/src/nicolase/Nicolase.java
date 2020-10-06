a91e8ce4470df0f936ca56eb3496b7047a14eb8a
Nicolas  Esquivel C <nicoescama@192.168.0.7>
2020-10-06 12:28:04 -0500

    include java methods for sorting and filtering

---
+++ nicolase/src/nicolase/Nicolase.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nicolase;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author nicoescama
+ */
+public class Nicolase {
+       
+    public static HashMap<Character, Number> getCaracteresRepetidos(String palabra){
+        
+        //chars that are not alphanumeric are removed
+        palabra = palabra.replaceAll("[^a-zA-Z0-9]", ""); 
+        HashMap<Character, Number> respuesta = new HashMap<>();
+                
+        Map<Character, Long> valores = palabra.toLowerCase().chars().mapToObj(e -> (char)e).
+                collect(Collectors.groupingBy(c -> c, Collectors.counting())).entrySet().stream()
+                .filter(c -> c.getValue() > 1).collect(Collectors.toMap(x -> x.getKey(), x -> x.getValue()));
+        System.out.println(valores);
+                
+        return respuesta;
+        
+    }
+    
+    public static Set<String> filtrar(Set<String> palabrasfiltrar, String filtroA){
+    	String filtroT = ".*"+ filtroA+ ".*";
+    	return palabrasfiltrar.stream().filter(p ->p.matches(filtroT)).collect(Collectors.toSet());
+        //return palabrasfiltrar.stream().filter(pattern.asPredicate()).collect(Collectors.toSet());
+        
+    }
+
+
+    public static void main(String[] args) {
+        
+        Set<String> palabras = new HashSet<>();
+        palabras.add("Nicolas");
+        palabras.add("Eliana");
+        palabras.add("Jorge12");
+        palabras.add("@@@Prueba1");
+        palabras.add("##@@");
+        
+        
+        //When sorted it is needed to be saved on a List to keep the order
+        //the method sorted() can have input of type Comparator in order to define the way of comparing items 
+        List<String> palabras_ordenadas = palabras.stream().sorted()
+        		.collect(Collectors.toList());
+        
+        String filtro = "[^a-zA-Z0-9]";
+        //To filter it is used the created method filtrar
+        //its input can be a pattern or a regex
+        Set<String> palabras_filtradas = filtrar(palabras, filtro);
+        
+        System.out.println(palabras_filtradas);
+        System.out.println(palabras_ordenadas);
+        
+        getCaracteresRepetidos("EliAAaAa$$a@@@@na11");
+        
+        
+        
+        
+        
+    }
+    
+}
